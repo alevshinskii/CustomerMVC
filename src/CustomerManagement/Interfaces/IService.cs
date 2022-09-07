a883ad111848f143d3f679d6112d7ce35acffab5
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-09-07 22:46:38 +0300

    feat: customer service added

---
+++ src/CustomerManagement/Interfaces/IService.cs
@@ -0,0 +1,12 @@
+﻿using CustomerManagement.Entities;
+
+namespace CustomerManagement.Interfaces;
+
+public interface IService<TEntity>
+{
+    TEntity? Get(int entityId);
+    TEntity? Create(TEntity entity);
+    bool Update(TEntity entity);
+    bool Delete(int entityId);
+    List<TEntity> GetAll();
+}
